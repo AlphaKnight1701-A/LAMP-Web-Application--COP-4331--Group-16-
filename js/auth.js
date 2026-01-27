869e0352cff47fcf0cee42d371cc49df50e4d7c6
HML786 <haanilalji2@gmail.com>
2026-01-27 15:14:37 -0500

    Add auth.js with placeholder functions

---
+++ js/auth.js
@@ -0,0 +1,22 @@
+const baseUrl = "http://poosproject.xyz/api";
+const extension = "php";
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function login() {
+    // TODO: Implement function
+}
+
+function logout() {
+    // TODO: Implement function
+}
+
+function saveCookie() {
+    // TODO: Implement function
+}
+
+function readCookie() {
+    // TODO: Implement function
+}
