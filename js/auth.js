b10e89f06b337be75485f100677fee4c3b2ce752
HML786 <haanilalji2@gmail.com>
2026-01-28 11:42:22 -0500

    Begin implementing login function

--- js/auth.js
+++ js/auth.js
@@ -6,7 +6,18 @@ let firstName = "";
 let lastName = "";
 
 function login() {
-    // TODO: Implement function
+    let username = document.getElementById("loginUsername").value;
+    let password = document.getElementById("loginPassword").value;
+
+    // The span tag that gives messages to the user
+    document.getElementById("loginResult").innerHTML = "";
+
+    let tmp = {
+        login: username,
+        password: password
+    }
+
+    let jsonPayload = JSON.stringify(tmp);
 }
 
 function logout() {
