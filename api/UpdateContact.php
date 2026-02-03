636439f1938c0ebafb1ffae182e46a14554c6def
Colin McLallen <co288701@ucf.edu>
2026-02-02 19:39:52 -0500

    Add files via upload

---
+++ api/UpdateContact.php
@@ -0,0 +1,63 @@
+<?php
+
+        $inputData = getRequestInfo();
+        $connection = new mysqli("localhost", "GOAT", "ILoveLamp", "COP4331");
+
+        // Declares and Sets Up Contact Detail Variables
+		$id = $inputData["id"];
+		$firstName = trim($inputData["firstName"]);
+		$lastName = trim($inputData["lastName"]);
+		$phone = trim($inputData["phone"]);
+		$email = trim($inputData["email"]);
+
+		// Sets, Prepares, and Executes Contact Update SQL Statement to DB
+		$sqlStatement = $connection->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ? WHERE ID = ?");
+		$sqlStatement->bind_param("ssssi", $firstName, $lastName, $phone, $email, $id);
+
+		// Condition Statement that Checks if Execution was Successful
+		if($sqlStatement->execute())
+		{
+			// Returns Updated Contact Info
+			returnWithInfo($id, $firstName, $lastName, $phone, $email, 0);
+		}
+		else // Execution Failed
+		{
+			// Returns Error Message
+			returnWithError("Failed to update contact");
+		}
+
+		// Closes Statement
+		$sqlStatement->close();
+        $connection->close();
+
+    // Function that Retrieves Input Data from Request
+	function getRequestInfo()
+	{
+		// Retrieve, Decodes, and Returns JSON Input Data
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    // Function that Returns with Contact Info
+	function returnWithInfo($id, $firstName, $lastName, $phone, $email, $userID)
+	{
+		// Constructs and Sends Contact Info JSON Object
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","phone":"' . $phone . '","email":"' . $email . '","userID":' . $userID . ',"error":""}';
+		sendResultInfoAsJson($retValue);
+	}
+
+    function returnWithError($err)
+	{
+		// Constructs and Sends Error JSON Object
+		$retValue = '{"id":0,"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+    // Function that Sends Result Info as JSON
+	function sendResultInfoAsJson($obj)
+	{
+		// Sets Header and Echoes JSON Object
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+?>
