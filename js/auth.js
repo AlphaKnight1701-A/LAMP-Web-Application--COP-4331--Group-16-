3c2e5fbc12e79520b56b81499b82f9c40bbbef7f
HML786 <haanilalji2@gmail.com>
2026-02-06 14:21:48 -0500

    Export logout function

--- js/auth.js
+++ js/auth.js
@@ -198,7 +198,7 @@ function searchContact()
 	
 }
 
-function logout() {
+export function logout() {
 	// Expiry date is UNIX epoch
     document.cookie = "firstName=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
 	// Go to index
