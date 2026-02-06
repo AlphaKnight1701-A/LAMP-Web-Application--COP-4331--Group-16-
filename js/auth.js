63ff1ba3b3b3e3878fd0c5f70ffa9c7ba34fe675
HML786 <haanilalji2@gmail.com>
2026-02-06 15:06:43 -0500

    Fix logout onclick handling on index.html

--- js/auth.js
+++ js/auth.js
@@ -259,11 +259,15 @@ function checkLogin() {
 			// Change login button to logout
 			document.getElementById("loginButton").innerHTML = "Logout";
 			document.getElementById("loginButtonLink").href = "javascript:void(0)"; // Don't link to any page
-			document.getElementById("loginButton").onclick = "logout();"; // Logout when pressed
+			document.getElementById("loginButton").onclick = () => {
+				logout();
+			};
 
 			// Change signup button to homepage
 			document.getElementById("signupButton").innerHTML = "Go to Homepage";
 			document.getElementById("signupButtonLink").href = "homepage.html";
+
+			// Show username
 			document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
 		}
 	}
