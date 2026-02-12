2a5efc82a45ff830095c1e1b5f05f87d4d8c9ad6
HML786 <haanilalji2@gmail.com>
2026-02-12 14:54:25 -0500

    Add delete confirmation dialog to contact-card

--- components/contact-card.js
+++ components/contact-card.js
@@ -30,16 +30,47 @@ class ContactCard extends HTMLElement {
                 <button type="button" class="inline-flex items-center justify-center w-12 h-12 rounded-md hover:bg-white hover:text-yellow-400 transition-colors" aria-label="Edit">
                     <span class="material-symbols-outlined">person_edit</span>
                 </button>
-                <button type="button" class="bg-red-600 inline-flex items-center justify-center w-12 h-12 rounded-md hover:bg-white hover:text-red-600 transition-colors" aria-label="Delete">
+                <button type="button" class="bg-red-600 inline-flex items-center justify-center w-12 h-12 rounded-md hover:bg-white hover:text-red-600 transition-colors" id="deleteButton" aria-label="Delete">
                     <span class="material-symbols-outlined">delete</span>
                 </button>
             </div>
         </div>
+
+        <!-- Delete dialog -->
+        <dialog id="deleteDialog" class="m-auto rounded-md p-0 border-none bg-transparent backdrop:bg-black/80">
+            <div class="flex flex-col p-10 justify-center items-center bg-[#2b2b2b] rounded-lg text-center">
+                <h1 class="text-xl font-bold text-white pb-3">Are you sure you want to delete this contact?</h1>
+                <p class="text-gray-400">${this.firstName} ${this.lastName}</p>
+                <p class="text-gray-400">${this.email}</p>
+                <p class="text-gray-400 pb-5">${this.phone}</p>
+                <div class="flex flex-col gap-3 px-5">
+                    <button id="confirmDelete" class="max-w-md w-full sm:w-md py-1 px-5 rounded-md bg-red-600 text-white hover:bg-white hover:text-red-600">Delete Contact</button>
+                    <button id="cancel" class="max-w-md w-full sm:w-md py-1 px-5 rounded-md bg-yellow-400 text-white hover:bg-white hover:text-yellow-400">Cancel</button>
+                </div>
+            </div>
+        </dialog>
         `;
+
+        // Delete dialog
+        const dialog = this.querySelector("#deleteDialog");
+
+        // Delete contact behavior
+        this.querySelector("#deleteButton").addEventListener("click", () => {
+            dialog.showModal();
+        });
+
+        this.querySelector("#cancel").addEventListener("click", () => {
+            dialog.close();
+        });
+
+        this.querySelector("#confirmDelete").addEventListener("click", () => {
+            // Close dialog when deleting
+            dialog.close();
+            // deleteContact();
+        });
     }
 
     // Getters and setters
-    
     // Used for deleting/updating contacts
     set contactId(value) {
         this.setAttribute('contact-id', value);
