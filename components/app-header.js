829cabe1dc052327e79f4624a1c4d8f6dbb3c9ed
HML786 <haanilalji2@gmail.com>
2026-02-17 15:40:47 -0500

    Use univeral hover style for buttons, change all yellow-400 to yellow-500

--- components/app-header.js
+++ components/app-header.js
@@ -7,7 +7,7 @@ class AppHeader extends HTMLElement {
         let loggedIn = isLoggedIn();
         this.innerHTML =
         `
-            <header class="bg-[#1c1c1c] text-yellow-400 shadow-md sticky top-0 z-50">
+            <header class="bg-[#1c1c1c] text-yellow-500 shadow-md sticky top-0 z-50">
                 <div class="max-w-7xl mx-auto px-6">
                 <div class="flex h-16 items-center justify-between">
                     <!-- Logo -->
@@ -18,7 +18,7 @@ class AppHeader extends HTMLElement {
                     <nav class="flex items-center space-x-6">
                         <a href="index.html" class="text-slate-300 hover:text-white transition">Home</a>
                         <a href="about.html" class="text-slate-300 hover:text-white transition">About</a>
-                        <a href="${loggedIn ? "javascript:void(0);" : "login.html"}" class="rounded-md px-4 py-2 bg-yellow-400 text-sm text-white font-bold hover:bg-white hover:text-yellow-400 transition" id="loginButton">
+                        <a href="${loggedIn ? "javascript:void(0);" : "login.html"}" class="rounded-md px-4 py-2 bg-yellow-500 text-sm text-white font-bold hover:bg-white hover:text-yellow-500 transition" id="loginButton">
                             ${loggedIn ? "Logout" : "Login"}
                         </a>
                     </nav>
--- components/contact-card.js
+++ components/contact-card.js
@@ -14,20 +14,20 @@ class ContactCard extends HTMLElement {
     render() {
         this.innerHTML = `
         <div class="flex flex-col md:flex-row justify-start items-center md:items-start bg-[#2b2b2b] text-white max-w-full w-full px-6 py-5 rounded-lg gap-4">
-            <span class="material-symbols-outlined text-yellow-400 text-6xl! shrink-0">account_circle</span>
+            <span class="material-symbols-outlined text-yellow-500 text-6xl! shrink-0">account_circle</span>
             <div class="flex flex-col flex-1 min-w-0 w-full text-center md:text-left">
                 <h1 class="font-bold text-2xl pb-2 wrap-break-word">${this.firstName} ${this.lastName}</h1>
                 <div class="flex flex-row justify-center md:justify-start items-center gap-2">
-                    <span class="material-symbols-outlined text-yellow-400 text-sm">mail</span>
+                    <span class="material-symbols-outlined text-yellow-500 text-sm">mail</span>
                     <p class="truncate hover:text-clip hover:whitespace-normal break-all text-sm md:text-base">${this.email}</p>
                 </div>
                 <div class="flex flex-row justify-center md:justify-start items-center gap-2">
-                    <span class="material-symbols-outlined text-yellow-400 text-sm">phone_in_talk</span>
+                    <span class="material-symbols-outlined text-yellow-500 text-sm">phone_in_talk</span>
                     <p class="wrap-break-word text-sm md:text-base">${this.phone}</p>
                 </div>
             </div>
             <div class="flex flex-row md:flex-col gap-2 mt-4 md:mt-0 md:ml-auto">
-                <button type="button" id="editButton" class="inline-flex items-center justify-center w-12 h-12 rounded-md hover:bg-white hover:text-yellow-400 transition-colors" aria-label="Edit">
+                <button type="button" id="editButton" class="inline-flex items-center justify-center w-12 h-12 rounded-md transition-colors" aria-label="Edit">
                     <span class="material-symbols-outlined">person_edit</span>
                 </button>
                 <button type="button" class="bg-red-600 inline-flex items-center justify-center w-12 h-12 rounded-md hover:bg-white hover:text-red-600 transition-colors" id="deleteButton" aria-label="Delete">
@@ -58,7 +58,7 @@ class ContactCard extends HTMLElement {
         <!-- Delete dialog -->
         <dialog id="deleteDialog" class="m-auto rounded-md px-3 border-none opacity-0 scale-80 transition-all duration-300 bg-transparent backdrop:bg-black/50 backdrop:backdrop-blur-sm">
             <div class="flex flex-col py-10 px-5 sm:px-10 justify-center items-center bg-[#2b2b2b] rounded-lg text-center">
-            <span class="material-symbols-outlined text-6xl! text-yellow-400 pb-3">warning</span>
+            <span class="material-symbols-outlined text-6xl! text-yellow-500 pb-3">warning</span>
                 <h1 class="text-xl font-bold text-white pb-3">Are you sure you want to delete this contact?</h1>
                 <div class="flex flex-row gap-2 items-center text-gray-400 max-w-full">
                     <span class="material-symbols-outlined">person</span>
@@ -74,7 +74,7 @@ class ContactCard extends HTMLElement {
                 </div>
                 <div class="flex flex-col gap-3 px-5">
                     <button id="confirmDelete" class="max-w-md w-full sm:w-md py-2 px-5 rounded-md font-bold bg-red-600 text-white hover:bg-white hover:text-red-600">Delete Contact</button>
-                    <button id="cancel" class="max-w-md w-full sm:w-md py-2 px-5 rounded-md font-bold bg-yellow-400 text-white hover:bg-white hover:text-yellow-400">Cancel</button>
+                    <button id="cancel" class="max-w-md w-full sm:w-md py-2 px-5 rounded-md font-bold">Cancel</button>
                 </div>
             </div>
         </dialog>
